4a30e09f37f8d884983d5e68b7d648802948b2a5
Nazar Annakhalov <nazar.annahalov@yandex.com>
2023-08-21 14:53:01 +0300

    initial

---
+++ audio_downloader.py
@@ -0,0 +1,42 @@
+import os
+from pytube import Playlist
+from moviepy.editor import AudioFileClip
+import re
+import unicodedata
+
+# Specify the URL of the YouTube playlist
+playlist_url = "https://www.youtube.com/playlist?list=PL3GQVNZapamNVePI8sxa1vE_XonO2bQ1n"
+
+# Create a Playlist object
+playlist = Playlist(playlist_url)
+playlist_name = playlist.title
+playlist_name = playlist_name.replace(" ", "_").lower()
+
+# Specify the directory where you want to save the audio files
+output_dir = os.path.join("audio", playlist_name)
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# Download each video's audio and convert to MP3
+for video in playlist.videos:
+    # Get the audio stream
+    audio_stream = video.streams.filter(only_audio=True).first()
+    
+    if audio_stream:
+        print(f"Downloading audio: {video.title}")
+        filename = f"{video.title.replace(' ', '_').lower()}.mp4"
+        audio_path = os.path.join(output_dir, filename)
+        audio_stream.download(output_path=output_dir, filename=filename)
+        
+        # Convert the audio to MP3 using moviepy
+        audio_clip = AudioFileClip(audio_path)
+        filename = video.title
+        audio_mp3_path = os.path.join(output_dir, f"{filename}.mp3")
+        audio_clip.write_audiofile(audio_mp3_path, codec='mp3', ffmpeg_params=["-ac", "2"])
+        
+        # Clean up the temporary webm file
+        audio_clip.close()
+        os.remove(audio_path)
+
+print("Download and conversion to MP3 complete!")
---
+++ playlist_downloader.py
@@ -0,0 +1,41 @@
+import os
+from pytube import Playlist
+from pytube.exceptions import PytubeError
+
+# Specify the URL of the YouTube playlist
+playlist_url = "https://www.youtube.com/playlist?list=PLtPJ9lKvJ4oiNMvYbOzCmWy6cRzYAh9B1"
+
+# Create a Playlist object
+playlist = Playlist(playlist_url)
+playlist_name = playlist.title
+playlist_name = playlist_name.replace(" ", "_").lower()
+
+# Specify the directory where you want to save the videos
+output_dir = os.path.join("videos", playlist_name)
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# Function to handle retries for download
+def download_with_retry(stream, output_path, retry_attempts=3):
+    for attempt in range(retry_attempts):
+        try:
+            print(f"Downloading: {video.title}")
+            stream.download(output_path=output_path)
+            print("Download successful!")
+            return True
+        except Exception as e:
+            print(f"An error occurred (Attempt {attempt + 1}/{retry_attempts}): {str(e)}")
+    
+    print("Download failed after all retry attempts.")
+    return False
+
+# Download each video with retry
+for video in playlist.videos:
+    # Get the highest resolution stream
+    highest_resolution_stream = video.streams.get_highest_resolution()
+    
+    if highest_resolution_stream:
+        download_with_retry(highest_resolution_stream, output_dir)
+
+print("All downloads complete!")
