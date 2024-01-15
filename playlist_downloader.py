3919b5b9a274b9ee092ca568e1d7f64db19c33b7
Nazar Annakhalov <nazar.annahalov@yandex.com>
2024-01-15 19:55:48 +0500

    codestyle fix in playlist_downloader.py

--- playlist_downloader.py
+++ playlist_downloader.py
@@ -4,7 +4,9 @@ from pytube.exceptions import PytubeError
 from tqdm import tqdm
 
 # Specify the URL of the YouTube playlist
-playlist_url = "https://www.youtube.com/playlist?list=PLuhp4lHyeGyJ88MpWHbJJewswLrUyLW5o"
+playlist_url = (
+    "https://www.youtube.com/playlist?list=PLuhp4lHyeGyJ88MpWHbJJewswLrUyLW5o"
+)
 
 # Create a Playlist object
 playlist = Playlist(playlist_url)
@@ -17,6 +19,7 @@ output_dir = os.path.join("videos", playlist_name)
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
+
 # Function to handle retries for download
 def download_with_retry(stream, output_path, retry_attempts=3):
     for attempt in range(retry_attempts):
@@ -26,10 +29,13 @@ def download_with_retry(stream, output_path, retry_attempts=3):
             print("Download successful!")
             return True
         except Exception as e:
-            print(f"An error occurred (Attempt {attempt + 1}/{retry_attempts}): {str(e)}")
-    
+            print(
+                f"An error occurred (Attempt {attempt + 1}/{retry_attempts}): {str(e)}"
+            )
+
     print("Download failed after all retry attempts.")
     return False
+
 
 # Function to update the progress bar
 def on_progress(stream, chunk, bytes_remaining):
@@ -38,14 +44,17 @@ def on_progress(stream, chunk, bytes_remaining):
     progress = bytes_downloaded / total_size * 100
     pbar.update(progress - pbar.n)
 
+
 # Download each video with retry
 for video in playlist.videos:
     # Get the highest resolution stream
     highest_resolution_stream = video.streams.get_highest_resolution()
-    
+
     if highest_resolution_stream:
         # Initialize the progress bar
-        pbar = tqdm(total=100, desc=f"Downloading {video.title}", unit="%", unit_scale=True)
+        pbar = tqdm(
+            total=100, desc=f"Downloading {video.title}", unit="%", unit_scale=True
+        )
         download_with_retry(highest_resolution_stream, output_dir)
         pbar.close()
 
